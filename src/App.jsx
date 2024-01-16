9b5760c3d5954c5a127b37b8cea4169d0e8523d5
CristianSSS <cristian.salgado4756@gmail.com>
2024-01-15 21:25:09 -0300

    add note

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
 
     <div className=' relative'>
-   
+   holaaaaaaaaaaaaaaaaa
     <div className='container mx-auto h-auto md:h-screen min-h-600'>
       
       <Header/>
