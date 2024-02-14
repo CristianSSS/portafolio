17e64db68c76c13059afdcaf2d784cce15175b54
Crissalgado <cristian.salgado4756@gmail.com>
2024-02-14 14:47:59 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,25 @@ import Home from './pages/home';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import {getFirestore, doc, getDoc } from "firebase/firestore"; 
+import { useEffect } from 'react';
+
 export default function App() {
 
+
+  useEffect(()=>{
+
+    const DB = getFirestore();
+
+    const item = doc(DB, 'works', '8TaQziS2s9kWm70rGBlR');
+
+    getDoc(item)
+    .then(snapshot =>{
+      console.log(snapshot)
+    })
+
+    
+  }, [])
 
   return (
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg8Xz09VWFAFw9COFzIvjM4X165uCswbM",
+  authDomain: "portafolio-e028e.firebaseapp.com",
+  projectId: "portafolio-e028e",
+  storageBucket: "portafolio-e028e.appspot.com",
+  messagingSenderId: "887568634589",
+  appId: "1:887568634589:web:731fc1506324158f9f0134"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
